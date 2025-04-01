2a81db9b03c6c39782428cbf5d540ebf0c2c0fec
Umariy Jaloliddin <j.umariy@uic.group>
2025-04-01 15:14:12 +0500

    add: the code is added

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("for first commit")
+import easyocr
+
+reader = easyocr.Reader(['en'])
+
+# Read the processed image
+result = reader.readtext('images/1.png',
+                       width_ths=1,
+                       decoder='beamsearch',
+                       contrast_ths=0.3,
+                       adjust_contrast=0.7)
+
+for (bbox, text, prob) in result:
+    print(text)
